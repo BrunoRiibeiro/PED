71b9500696e9f63657249e5adcfc77d588c5a99b
Bruno Ribeiro <bbrunoo@icloud.com>
2023-01-16 19:35:59 -0300

    VHDL experiment 3

--- exp_3/exp_3.srcs/sources_1/new/exp_3.vhd
+++ exp_3/exp_3.srcs/sources_1/new/exp_3.vhd
@@ -6,26 +6,30 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity exp_3 is
     Port ( a, b : in STD_LOGIC_VECTOR(4 downto 0);
            s    : in STD_LOGIC_VECTOR(1 downto 0);
-           f    : out STD_LOGIC_VECTOR(4 downto 0));
-           over : in STD_LOGIC;
+           f    : out STD_LOGIC_VECTOR(4 downto 0);
+           over : out STD_LOGIC);
 end exp_3;
 
 architecture Behavioral of exp_3 is
+    signal sf: STD_LOGIC_VECTOR(4 downto 0);
 begin
     process(a, b, s)
     begin
         case s is
             when "00" =>
-                f <= a + b;
-                over <= (not a(4) and not b(4) and f(4)) or (a and b and not c);
+                sf <= a + b;
+                over <= (not a(4) and not b(4) and sf(4)) or (a(4) and b(4) and not sf(4));
+                f <= sf;
             when "01" =>
-                f <= a - b;
-                over <= (not a(4) and not b(4) and f(4)) or (a and b and not c);
+                sf <= a - b;
+                over <= (not a(4) and not b(4) and sf(4)) or (a(4) and b(4) and not sf(4));
+                f <= sf;
             when "10" => f <= a and b;
             when "11" => f <= a or b;
         end case;
